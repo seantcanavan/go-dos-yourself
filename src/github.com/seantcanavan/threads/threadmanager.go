af67e36ddb6f77161264b5f58b17a0c2686cdcb1
Sean Thomas Canavan <seacanav@cisco.com>
2016-04-25 02:02:15 -0500

    restructing fixed bug on main event loop with value comparison begun interfacing threadmanager initial research into channels

--- src/github.com/seantcanavan/threads/threadmanager.go
+++ src/github.com/seantcanavan/threads/threadmanager.go
@@ -1,7 +1,39 @@
 package threads
 
-import "fmt"
 
-func PrintItem() {
-	fmt.Println("external print")
+type thread struct {
+	messages chan string
+	id int
+	name string
+}
+
+func NewThread(messages chan string, id int, name string) *thread {
+	t := new(thread)
+	t.messages = messages
+	t.id = id
+	t.name = name
+	return t
+}
+
+func (t thread) start() {
+	t.messages <- "start real"
+}
+
+func (t thread) stop() {
+	t.messages <- "stop real"
+}
+
+func (t thread) change_job() {
+	t.messages <- "change_job real"
+}
+
+func (t thread) print_details() {
+	t.messages <- "print_details real"
+}
+
+type thread_actions interface {
+	start()
+	stop()
+	change_job()
+	print_details()
 }
--- src/github.com/seantcanavan/threads/threadmanager_test.go
+++
@@ -1,9 +0,0 @@
-package threads
-
-import "fmt"
-import "testing"
-
-func Test(t *testing.T) {
-	fmt.Println("test executed")
-	fmt.Println("run 'go test' from within 'github.com/seantcanavan/threads'")
-}
